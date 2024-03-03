937b5c66d94dd54c59254ea69315db8d9c146679
fatihyd <yildizafatih@gmail.com>
2024-03-03 15:23:02 +0300

    style fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom"
 
 export default function Home() {
     return (
-        <>
+        <div className="home-container">
             <h1>Welcome to the store!</h1>
             <button>
                 <Link to="store">Shop now</Link>
             </button>
-        </>
+        </div>
     );
 }
